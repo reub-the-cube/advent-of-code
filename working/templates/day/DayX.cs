973bb20d73475bfee3830b4b528239c9b186bc49
Reuben <reuben.hillyer@hotmail.co.uk>
2022-11-27 20:59:09 +0000

    WIP: template for new day

---
+++ working/templates/day/DayX.cs
@@ -0,0 +1,19 @@
+﻿using AoC.Core;
+using day.models;
+
+namespace day;
+public class DayX :IDay
+{
+    private readonly IParser<Input> _parser;
+
+    public DayX(IParser<Input> parser)
+    {
+        _parser = parser;
+    }
+
+    public (int AnswerOne, int AnswerTwo) CalculateAnswers(string[] input)
+    {
+        var parsedInput = _parser.ParseInput(input);
+        throw new NotImplementedException();
+    }
+}
