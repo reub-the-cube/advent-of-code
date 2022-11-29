815ace4a2ced36d0b970a407408e6005c6951f71
Reuben <reuben.hillyer@hotmail.co.uk>
2022-11-29 21:03:19 +0000

    Add day4 input and make it easier for future days

--- Aoc.Day04.Tests/Day4Tests.cs
+++ Aoc.Day04.Tests/Day4Tests.cs
@@ -1,28 +1,41 @@
+using FluentAssertions;
+
 namespace aoc.day04.tests;
 
 public class Day4Tests
 {
-    private readonly string[] INPUT = new string[] { };
+    private readonly string[] INPUT = Array.Empty<string>();
+
+    public Day4Tests()
+    {
+        INPUT = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"..\\..\\..\\..\\AoC.Console\\day4input.txt"));
+    }
+
+    [Fact]
+    public void InputLoadsCorrectly()
+    {
+        INPUT.Length.Should().BeGreaterThan(0);
+    }
 
     [Fact]
     public void CalculatedAnswerOneMatchesTestCase()
     {
         var parser = new Parser();
-        var day2 = new Day4(parser);
+        var day4 = new Day4(parser);
 
-        var (answerOne, _) = day2.CalculateAnswers(INPUT);
+        var (answerOne, _) = day4.CalculateAnswers(INPUT);
 
-        answerOne.Should().Be(150);
+        answerOne.Should().Be(4512);
     }
 
     [Fact]
     public void CalculatedAnswerTwoMatchesTestCase()
     {
         var parser = new Parser();
-        var day2 = new Day4(parser);
+        var day4 = new Day4(parser);
 
-        var (_, answerTwo) = day2.CalculateAnswers(INPUT);
+        var (_, answerTwo) = day4.CalculateAnswers(INPUT);
 
-        answerTwo.Should().Be(900);
+        answerTwo.Should().Be(0);
     }
 }
