a1ab1791d8501d0b636d2b32acd4cce78faa0c4f
Reuben <reuben.hillyer@hotmail.co.uk>
2022-12-03 23:15:18 +0000

    Make the console more useable, add content to the readme

--- AoC.Console/Program.cs
+++ AoC.Console/Program.cs
@@ -4,60 +4,193 @@ using AoC.Console;
 using AoC.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 
 using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) =>
         services.ConfigureDayServices())
     .Build();
 
-RequestChallengeInput();
-
-void RequestChallengeInput()
+// Could do this intelligently with automation
+var implementedYearsAndDays = new Dictionary<int, List<int>>()
 {
-    Console.WriteLine("Which day's challenges would you like to run? Use a number from 1 to 25.");
-    var dayNumberInput = Console.ReadLine();
-    var (IsValid, Value) = ValidateDayNumber(dayNumberInput);
-    if (IsValid) InitialiseChallenge(2022, Value);
+    { 2021, new List<int> { 1, 2, 3, 4 } },
+    { 2022, new List<int> { 1, 2, 3, 4 } }
+};
+
+ParseArgs();
+
+void ParseArgs()
+{
+    int todayIndex = Array.IndexOf(args, "--today");
+    int allIndex = Array.IndexOf(args, "--all");
+    int yearArgIndex = Array.IndexOf(args, "--year");
+    int dayArgIndex = Array.IndexOf(args, "--day");
+
+    if (todayIndex > -1)
+    {
+        RunToday();
+    }
+    else if (allIndex > -1)
+    {
+        RunAll();
+    }
+    else if (yearArgIndex > -1)
+    {
+        RunYear(yearArgIndex, dayArgIndex);
+    }
+    else if (dayArgIndex > -1)
+    {
+        Console.WriteLine("Argument --day cannot be used without the --year argument.");
+    }
+    else
+    {
+        PromptForYearAndDay();
+    }
+}
+
+void RunAll()
+{
+    if (args.Length > 1)
+    {
+        Console.WriteLine("Argument --all cannot be used with any other arguments.");
+    }
+    else
+    {
+        PrepareAll();
+    }
+}
+
+void RunYear(int yearArgIndex, int dayArgIndex)
+{
+    string yearArg = yearArgIndex > -1 ? args[yearArgIndex + 1] : string.Empty;
+
+    var (YearIsValid, YearValue) = ValidateNumber(yearArg, implementedYearsAndDays.Keys.Min(), implementedYearsAndDays.Keys.Max());
+
+    if (YearIsValid)
+    {
+        if (dayArgIndex == -1)
+        {
+            // No --day arg, run the whole year
+            PrepareYear(YearValue);
+        }
+        else
+        {
+            var (DayIsValid, DayValue) = ValidateNumber(args[dayArgIndex + 1], 1, 25);
+            if (DayIsValid) PrepareDay(YearValue, DayValue);
+        }
+    }
+}
+
+void RunToday()
+{
+    int year = DateTime.Now.Year;
+    int day = DateTime.Now.Day;
+    PrepareDay(year, day);
+}
+
+void PromptForYearAndDay()
+{
+    Console.WriteLine($"Which year's challenges would you like to run? Use `all` or a number from {implementedYearsAndDays.Keys.Min()} to {implementedYearsAndDays.Keys.Max()}.");
+    var yearNumberInput = Console.ReadLine();
+
+    if (yearNumberInput == "all")
+    {
+        PrepareAll();
+    }
+    else
+    {
+        var (YearIsValid, YearValue) = ValidateNumber(yearNumberInput, implementedYearsAndDays.Keys.Min(), implementedYearsAndDays.Keys.Max());
+
+        if (YearIsValid)
+        {
+            Console.WriteLine("Which day's challenges would you like to run? Use `all` or a number from 1 to 25.");
+            var dayNumberInput = Console.ReadLine();
+
+            if (dayNumberInput == "all")
+            {
+                PrepareYear(YearValue);
+            }
+            else
+            {
+                var (DayIsValid, DayValue) = ValidateNumber(dayNumberInput, 1, 25);
+
+                if (DayIsValid) PrepareDay(YearValue, DayValue);
+            }
+
+        }
+    }
 
     Console.WriteLine("Press any key to finish, or press return to play again.");
     if (Console.ReadKey().Key == ConsoleKey.Enter)
     {
         Console.WriteLine();
-        RequestChallengeInput();
+        PromptForYearAndDay();
     }
 }
 
-void InitialiseChallenge(int year, int day)
+void PrepareAll()
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    foreach (var year in implementedYearsAndDays.Keys)
+    {
+        foreach (var day in implementedYearsAndDays[year])
+        {
+            SolveDay(year, day);
+        }
+    }
+    stopwatch.Stop();
+    Console.WriteLine($"Elapsed time (ms): {stopwatch.ElapsedMilliseconds}");
+}
+
+void PrepareYear(int year)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    foreach (var day in implementedYearsAndDays[year])
+    {
+        SolveDay(year, day);
+    }
+    stopwatch.Stop();
+    Console.WriteLine($"Elapsed time (ms): {stopwatch.ElapsedMilliseconds}");
+}
+
+void PrepareDay(int year, int day)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    SolveDay(year, day);
+    stopwatch.Stop();
+    Console.WriteLine($"Elapsed time (ms): {stopwatch.ElapsedMilliseconds}");
+}
+
+void SolveDay(int year, int day)
 {
     try
     {
-        var dayFormatted = day.ToString("00");
         var dayServiceResolver = host.Services.GetService<Func<(int, int), IDaySolver>>() ?? throw new InvalidOperationException("Day service resolver not found.");
-        var dayProcesser = dayServiceResolver((year, day));
-        var input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{year}", $"day{dayFormatted}input.txt"));
-        var (AnswerOne, AnswerTwo) = dayProcesser.CalculateAnswers(input);
+        var daySolver = dayServiceResolver((year, day));
+        var dayFormatted = day.ToString("00");
 
-        Console.WriteLine($"Final answer for day {dayFormatted}, challenge 1: {AnswerOne}");
-        Console.WriteLine($"Final answer for day {dayFormatted}, challenge 2: {AnswerTwo}");
+        var input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{year}", $"day{dayFormatted}input.txt"));
+        var (AnswerOne, AnswerTwo) = daySolver.CalculateAnswers(input);
+
+        Console.WriteLine($"Final answer for year {year}, day {dayFormatted}, challenge 1: {AnswerOne}");
+        Console.WriteLine($"Final answer for year {year}, day {dayFormatted}, challenge 2: {AnswerTwo}");
     }
-    catch (NotImplementedException nie)
+    catch (NotImplementedException)
     {
-        Console.WriteLine($"{nie.Message}");
+        Console.WriteLine($"Year {year}, day {day} not implemented");
     }
 }
 
-(bool IsValid, int Value) ValidateDayNumber(string? number)
+(bool IsValid, int Value) ValidateNumber(string? number, int min, int max)
 {
     var isValid = int.TryParse(number, out var parsedNumber);
-    
-    switch (parsedNumber)
+
+    if ((parsedNumber < min) || (parsedNumber > max))
     {
-        case < 1 or > 25:
-            Console.WriteLine("This input was not valid. Please try again.");
-            isValid = false;
-            break;
+        Console.WriteLine("This input was not valid. Please try again.");
+        isValid = false;
     }
 
     return (isValid, parsedNumber);
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# advent-of-code-2021
+# advent-of-code
+
+## Run what you've got
+
+From the `.\AoC.Console\` directory
+- Run today's challenges
+``` shell
+dotnet run --today
+```
+
+- Run all challenges
+``` shell
+dotnet run --all
+```
+
+- Run a year's worth of challenges
+``` shell
+dotnet run --year ####
+```
+
+- Run a specific day's challenges
+``` shell
+dotnet run --year #### --day ##
+```
+
+- Choose a year and day through the shell, and repeat
+``` shell
+dotnet run
+```
+
+## Project scaffolding for a new day
+
+The `.\Scaffolding` directory contains:
+- An `AoC.Core` directory which contains files that are shared between days and challenges and takes the form of a `AoC.Core` NuGet package
+- A template directory which contains a template called `aoc-dayx`, which can be used for setting up a new day
+
+From the root directory, you will need to run `.\update-scaffolding.ps1` if you want to:
+- Add a new day for the first time (so the NuGet gets published and the dotnet template gets installed)
+- Make changes to the NuGet (the new version should be updated in the `day.csproj` too)
+- Make changes to the custom template
+
+This file will:
+- Optionally try to pack and push the version specified of the NuGet
+- Install the latest version of the custom template
+- TODO: remove hard-coding for local NuGet feed
+
+## Add a new day
+
+Before you can add a new day for the first time, you need to install a dotnet template locally, as above.
+
+Then, from the root directory, run `.\add-new-day.ps1 -year #### -day ##`. This will:
+- Create *empty* files for the day's input (one for tests, one for submission)
+- Create a solution file for the year if it doesn't exist
+- Create a project from the `aoc-dayx` dotnet template
+- Create a test project using the xunit framework
+- Add the projects to the year's solution file
+- Add some boilerplate test code and install missing packages
+- Replace some `DayX` code with more appropriate names
