de94b6ebf635434407fd66d4a64e67d66a77a8c0
Reuben <reuben.hillyer@hotmail.co.uk>
2022-12-04 19:56:13 +0000

    Add scaffolding for day05

---
+++ 2022/aoc2022.day05/ServiceCollection.cs
@@ -0,0 +1,16 @@
+using AoC.Core;
+using aoc2022.day05.domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aoc2022.day05
+{
+    public static class ServiceCollection
+    {
+        public static IServiceCollection ConfigureDay05Services(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<Day05Solver>()
+                .AddScoped<IParser<Input>, Parser>();
+        }
+    }
+}
--- AoC.Console/ServiceCollection2022.cs
+++ AoC.Console/ServiceCollection2022.cs
@@ -2,6 +2,7 @@
 using aoc._2022.day02;
 using aoc._2022.day03;
 using aoc._2022.day04;
+using aoc2022.day05;
 using AoC.Core;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,7 +16,8 @@ namespace AoC.Console
                 .ConfigureDay01Services()
                 .ConfigureDay02Services()
                 .ConfigureDay03Services()
-                .ConfigureDay04Services();
+                .ConfigureDay04Services()
+                .ConfigureDay05Services();
         }
 
         public static IDaySolver ResolveDayFor2022(this IServiceProvider serviceProvider, int day)
@@ -26,6 +28,7 @@ namespace AoC.Console
                 2 => serviceProvider.GetService<Day02Solver>() ?? throw new InvalidOperationException(),
                 3 => serviceProvider.GetService<Day03Solver>() ?? throw new InvalidOperationException(),
                 4 => serviceProvider.GetService<Day04Solver>() ?? throw new InvalidOperationException(),
+                5 => serviceProvider.GetService<Day05Solver>() ?? throw new InvalidOperationException(),
                 _ => throw new NotImplementedException($"Day service provider has not been configured for day {day} this year.")
             };
             return daySolver;
