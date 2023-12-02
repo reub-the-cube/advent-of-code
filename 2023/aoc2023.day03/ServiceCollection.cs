6eb96181897b0075ee32eaa2fe1349c014674534
Reuben <reuben.hillyer@hotmail.co.uk>
2023-12-02 21:29:36 +0000

    2023 day 3 scaffolding

---
+++ 2023/aoc2023.day03/ServiceCollection.cs
@@ -0,0 +1,16 @@
+using AoC.Core;
+using aoc2023.day03.domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aoc2023.day03
+{
+    public static class ServiceCollection
+    {
+        public static IServiceCollection ConfigureDay03Services(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<Day03Solver>()
+                .AddScoped<IParser<Input>, Parser>();
+        }
+    }
+}
--- AoC.Console/ServiceCollection2023.cs
+++ AoC.Console/ServiceCollection2023.cs
@@ -1,6 +1,7 @@
 using AoC.Core;
 using aoc2023.day01;
 using aoc2023.day02;
+using aoc2023.day03;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AoC.Console
@@ -11,7 +12,8 @@ namespace AoC.Console
         {
             return services
                 .ConfigureDay01Services()
-                .ConfigureDay02Services();
+                .ConfigureDay02Services()
+                .ConfigureDay03Services();
         }
 
         public static IDaySolver ResolveDayFor2023(this IServiceProvider serviceProvider, int day)
@@ -20,6 +22,7 @@ namespace AoC.Console
             {
                 1 => serviceProvider.GetService<Day01Solver>() ?? throw new InvalidOperationException(),
                 2 => serviceProvider.GetService<Day02Solver>() ?? throw new InvalidOperationException(),
+                3 => serviceProvider.GetService<Day03Solver>() ?? throw new InvalidOperationException(),
                 _ => throw new NotImplementedException($"Day service provider has not been configured for day {day} this year.")
             };
         }
