a61ede5e9b02550a0243f776a379689ff3c6b878
Reuben <reuben.hillyer@hotmail.co.uk>
2023-12-03 05:22:04 +0000

    2023 day 3 add test input

--- 2023/aoc2023.day03.tests/Day03SolverTests.cs
+++ 2023/aoc2023.day03.tests/Day03SolverTests.cs
@@ -5,7 +5,7 @@ namespace aoc2023.day03.tests;
 public class Day03SolverTests
 {
     private readonly string[] INPUT = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "..", "Inputs", "2023", "day03testinput.txt"));
-    private const string EXPECTED_ANSWER_ONE = "not_implemented"; // <--------- solution from web page test example goes here
+    private const string EXPECTED_ANSWER_ONE = "4361"; // <--------- solution from web page test example goes here
     private const string EXPECTED_ANSWER_TWO = "not_implemented"; // <--------- solution from web page test example goes here
 
     [Fact]
