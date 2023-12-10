8fa62305393e4b4a485cbd2de3f91c5f5511ee6e
Reuben <reuben.hillyer@hotmail.co.uk>
2023-12-10 06:22:23 +0000

    2023 day 10 number of connected pipes can be counted

---
+++ 2023/aoc2023.day10.tests/TileTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+
+namespace aoc2023.day10.tests
+{
+    public class TileTests
+    {
+        [Fact]
+        public void NumberOfConnectedNeighboursIsZeroIfUnconnected()
+        {
+            var tile = new Tile(null, null, null, null);
+
+            var connectedPipes = tile.GetNumberOfConnectedNeighbours();
+
+            connectedPipes.Should().Be(0);
+        }
+
+        [Fact]
+        public void NumberOfConnectedNeighboursIsZeroIfNeighboursConnectElsewhere()
+        {
+            var tile = new Tile('-', '|', 'B', 'S');
+
+            var connectedPipes = tile.GetNumberOfConnectedNeighbours();
+
+            connectedPipes.Should().Be(0);
+        }
+
+        [Fact]
+        public void NumberOfConnectedNeighboursIsOneIfANeighbourIsConnectedToTheRight()
+        {
+            var tile = new Tile('-', '-', 'B', 'S');
+
+            var connectedPipes = tile.GetNumberOfConnectedNeighbours();
+
+            connectedPipes.Should().Be(1);
+        }
+
+        [Fact]
+        public void NumberOfConnectedNeighboursIsFourIfANeighbourIsConnectedOnAllSides()
+        {
+            var tile = new Tile('F', '7', 'J', 'L');
+
+            var connectedPipes = tile.GetNumberOfConnectedNeighbours();
+
+            connectedPipes.Should().Be(4);
+        }
+    }
+}
---
+++ 2023/aoc2023.day10/Tile.cs
@@ -0,0 +1,48 @@
+﻿namespace aoc2023.day10
+{
+    public class Tile
+    {
+        private readonly char? pipeAbove;
+        private readonly char? pipeRight;
+        private readonly char? pipeBelow;
+        private readonly char? pipeLeft;
+
+        public Tile(char? pipeAbove, char? pipeRight, char? pipeBelow, char? pipeLeft)
+        {
+            this.pipeAbove = pipeAbove;
+            this.pipeRight = pipeRight;
+            this.pipeBelow = pipeBelow;
+            this.pipeLeft = pipeLeft;
+        }
+
+        public int GetNumberOfConnectedNeighbours()
+        {
+            return NumberOfConnectionsAbove() + NumberOfConnectionsRight() + NumberOfConnectionsBelow() + NumberOfConnectionsLeft();
+        }
+
+        private int NumberOfConnectionsAbove()
+        {
+            return IsConnected(pipeAbove, new char[] {'7', '|', 'F'});
+        }
+
+        private int NumberOfConnectionsRight()
+        {
+            return IsConnected(pipeRight, new char[] { 'J', '-', '7' });
+        }
+
+        private int NumberOfConnectionsBelow()
+        {
+            return IsConnected(pipeBelow, new char[] { 'J', '|', 'L' });
+        }
+
+        private int NumberOfConnectionsLeft()
+        {
+            return IsConnected(pipeLeft, new char[] { 'L', '-', 'F' });
+        }
+
+        private static int IsConnected(char? adjacentPipe, char[] possibleConnections)
+        {
+            return Array.IndexOf(possibleConnections, adjacentPipe) > -1 ? 1 : 0;
+        }
+    }
+}
