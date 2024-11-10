18764c6cde513e8718d3fefc9a967fd0fd9d7333
Reuben <reuben.hillyer@hotmail.co.uk>
2024-11-10 21:17:09 +0000

    Add 2021 day 5 scaffolding

--- AoC.Console/ServiceCollection2021.cs
+++ AoC.Console/ServiceCollection2021.cs
@@ -3,6 +3,7 @@ using AoC.Core;
 using AoC.Day01;
 using AoC.Day02;
 using AoC.Day03;
+using aoc2021.day05;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AoC.Console
@@ -15,7 +16,8 @@ namespace AoC.Console
                 .ConfigureDay1Services()
                 .ConfigureDay2Services()
                 .ConfigureDay3Services()
-                .ConfigureDay4Services();
+                .ConfigureDay4Services()
+                .ConfigureDay05Services();
         }
 
         public static IDaySolver ResolveDayFor2021(this IServiceProvider serviceProvider, int day)
@@ -26,6 +28,7 @@ namespace AoC.Console
                 2 => serviceProvider.GetService<Day2>() ?? throw new InvalidOperationException(),
                 3 => serviceProvider.GetService<Day3>() ?? throw new InvalidOperationException(),
                 4 => serviceProvider.GetService<Day4>() ?? throw new InvalidOperationException(),
+                5 => serviceProvider.GetService<Day05Solver>() ?? throw new InvalidOperationException(),
                 _ => throw new NotImplementedException($"Day service provider has not been configured for day {day} this year.")
             };
         }
